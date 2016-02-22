47eab289d6f913133540a6b4d7cd50421101d13a
Ian Auld <imauld@gmail.com>
2016-02-21 21:09:26 -0800

    Fixed Remove

--- src/linkedlist/linkedlist.go
+++ src/linkedlist/linkedlist.go
@@ -73,19 +73,42 @@ func (l *List) Size() int {
 }
 
 // Remove ...
+func (l *List) Remove(node *Node) {
+	var previous *Node
+	for current := l.head; current != nil; current = current.next {
+		if current == node {
+			if current == l.head {
+				l.head = current.next
+			}
+			if previous != nil {
+				previous.next = current.next
+			}
+			current.next = nil
+			break
+		}
+		previous = current
+	}
+}
 
 func main() {
 	node := Node{10, nil}
-	node2 := Node{15, nil}
-	list := &List{&node2}
-	list.Insert(&node)
+	node2 := Node{20, nil}
+	node3 := Node{20, nil}
+	list := &List{&node}
+	list.Insert(&node2)
+	list.Insert(&node3)
 	fmt.Println(list)
-	fmt.Println(fmt.Sprintf("Value 10 in List: %v", list.Search(10)))
-	fmt.Println(fmt.Sprintf("Value 15 in List: %v", list.Search(15)))
-	fmt.Println(fmt.Sprintf("Value 20 in List: %v", list.Search(20)))
-	fmt.Println(list.Size())
-	fmt.Println("list.pop() = ", list.Pop())
-	fmt.Println(list.Size())
-	list.Insert(&Node{20, nil})
+	list.Remove(&node3)
 	fmt.Println(list)
+	// list.Insert(node2)
+	// fmt.Println(list)
+	// fmt.Println(fmt.Sprintf("Value 10 in List: %v", list.Search(10)))
+	// fmt.Println(fmt.Sprintf("Value 15 in List: %v", list.Search(15)))
+	// fmt.Println(fmt.Sprintf("Value 20 in List: %v", list.Search(20)))
+	// fmt.Println(list.Size())
+	// fmt.Println("list.pop() = ", list.Pop())
+	// fmt.Println(list.Size())
+	// list.Insert(&Node{20, nil})
+	// fmt.Println(list)
+
 }
